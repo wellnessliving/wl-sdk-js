bde4837d6d4a682ce2f616408b431afc1e968ec9
Vladislav Kobzev <kp42@ya.ru>
2023-06-02 20:16:05 +0300

    wl-47755: Fix bugs from jira comment +review WL-45724

--- WellnessLiving/Wl/Profile/Form/Response/ResponseListModel.js
+++ WellnessLiving/Wl/Profile/Form/Response/ResponseListModel.js
@@ -1,7 +1,5 @@
 /**
- * List of user's quiz responses.
- *
- * This model is generated automatically based on API.
+ * Retrieves list of user's quiz responses.
  *
  * @augments WlSdk_ModelAbstract
  * @constructor
@@ -19,6 +17,9 @@ function Wl_Profile_Form_Response_ResponseListModel()
    * @typedef {{}} Wl_Profile_Form_Response_ResponseListModel_a_quiz_login
    * @property {string} dtl_date Date of the request to fill out a quiz form.
    * @property {number} id_source Place where request to fill out a quiz form occurred.
+   * @property {boolean} is_private Whether form can be viewed by staff member only after confirmation.
+   *    `true` if form can be viewed only after confirmation
+   *    `false` if form can be viewed always
    * @property {string} k_quiz Quiz key.
    * @property {string} k_quiz_login Key of the request.
    * @property {string} text_title Title of the quiz.
@@ -30,6 +31,12 @@ function Wl_Profile_Form_Response_ResponseListModel()
    *  <dd>Date of the request to fill out a quiz form.</dd>
    *  <dt>int <var>id_source</var></dt>
    *  <dd>Place where request to fill out a quiz form occurred.</dd>
+   *  <dt>bool <var>is_private</var></dt>
+   *  <dd>
+   *    Whether form can be viewed by staff member only after confirmation.
+   *    `true` if form can be viewed only after confirmation
+   *    `false` if form can be viewed always
+   *  </dd>
    *  <dt>string <var>k_quiz</var></dt>
    *  <dd>Quiz key.</dd>
    *  <dt>string <var>k_quiz_login</var></dt>
@@ -47,6 +54,9 @@ function Wl_Profile_Form_Response_ResponseListModel()
    * @typedef {{}} Wl_Profile_Form_Response_ResponseListModel_a_quiz_response
    * @property {string} dtl_date Date of a request to fill out a quiz form.
    * @property {number} id_source Place where request to fill out a quiz form occurred.
+   * @property {boolean} is_private Whether form can be viewed by staff member only after confirmation.
+   *    `true` if form can be viewed only after confirmation
+   *    `false` if form can be viewed always
    * @property {string} k_quiz Quiz key.
    * @property {string} k_quiz_login Quiz response key.
    * @property {string} text_title Title of the quiz.
@@ -58,6 +68,12 @@ function Wl_Profile_Form_Response_ResponseListModel()
    *  <dd>Date of a request to fill out a quiz form.</dd>
    *  <dt>int <var>id_source</var></dt>
    *  <dd>Place where request to fill out a quiz form occurred.</dd>
+   *  <dt>bool <var>is_private</var></dt>
+   *  <dd>
+   *    Whether form can be viewed by staff member only after confirmation.
+   *    `true` if form can be viewed only after confirmation
+   *    `false` if form can be viewed always
+   *  </dd>
    *  <dt>string <var>k_quiz</var></dt>
    *  <dd>Quiz key.</dd>
    *  <dt>string <var>k_quiz_login</var></dt>
@@ -70,6 +86,14 @@ function Wl_Profile_Form_Response_ResponseListModel()
    * @type {Wl_Profile_Form_Response_ResponseListModel_a_quiz_response[]}
    */
   this.a_quiz_response = [];
+
+  /**
+   * `true` if API is being used from backend, `false` otherwise.
+   *
+   * @get get
+   * @type {boolean}
+   */
+  this.is_backend = false;
 
   /**
    * Key of a business to show information for.
@@ -97,7 +121,7 @@ WlSdk_ModelAbstract.extend(Wl_Profile_Form_Response_ResponseListModel);
  */
 Wl_Profile_Form_Response_ResponseListModel.prototype.config=function()
 {
-  return {"a_field": {"a_quiz_login": {"get": {"result": true}},"a_quiz_response": {"get": {"result": true}},"k_business": {"get": {"get": true}},"uid": {"get": {"get": true}}}};
+  return {"a_field": {"a_quiz_login": {"get": {"result": true}},"a_quiz_response": {"get": {"result": true}},"is_backend": {"get": {"get": true}},"k_business": {"get": {"get": true}},"uid": {"get": {"get": true}}}};
 };
 
 /**
