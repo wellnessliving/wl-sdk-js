e2a62da66977a9d7b9e344758389a8eb41312055
Vladislav Kobzev <kp42@ya.ru>
2023-06-20 13:51:39 +0300

    wl-48282: PHP and JS SDK synchronization +review WL-46089

--- WellnessLiving/Core/Passport/Login/Enter/NotepadModel.js
+++ WellnessLiving/Core/Passport/Login/Enter/NotepadModel.js
@@ -43,6 +43,36 @@ function Core_Passport_Login_Enter_NotepadModel()
 WlSdk_ModelAbstract.extend(Core_Passport_Login_Enter_NotepadModel);
 
 /**
+ * Evaluates hash based on notepad and plain user password.
+ *
+ * @param {String} s_password Plain user password.
+ * @return {String} Password hash, depends on hash, solt, type of the hash and plain user password.
+ */
+Core_Passport_Login_Enter_NotepadModel.prototype.hash=function(s_password)
+{
+  return sha3_512(this.s_notepad+this.passwordHash(s_password));
+};
+
+/**
+ * Evaluates initial hash based on notepad and plain user password (this is value of hash that is stored in the database).
+ *
+ * <b>Be attentive!</b> This method has a PHP-side counterpart: {@link \Core\Passport\Login\PassportLogin::passwordHash()}.
+ *
+ * @param {String} s_password Plain user password.
+ * @return {String} Password hash that is stored in the database.
+ */
+Core_Passport_Login_Enter_NotepadModel.prototype.passwordHash=function(s_password)
+{
+  var a_delimiter=['r','4S','zqX','zqiOK','TLVS75V','Ue5aLaIIG75','uODJYM2JsCX4G','kt58wZfHHGQkHW4QN','Lh9Fl5989crMU4E7P6E'];
+  var s_hash='';
+
+  for(var i=0;i<9;i++)
+    s_hash=s_hash+a_delimiter[i]+s_password;
+
+  return sha3_512(s_hash);
+};
+
+/**
  * @inheritDoc
  */
 Core_Passport_Login_Enter_NotepadModel.prototype.config=function()
