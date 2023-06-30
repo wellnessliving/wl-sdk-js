f06c28d24803ad95d87ccc39840d160d4f0ebef7
Vladislav Kobzev <kp42@ya.ru>
2023-06-30 23:00:57 +0300

    wl-48282: PHP and JS SDK synchronization +review WL

---
+++ WellnessLiving/Core/Quiz/ElementSid.js
@@ -0,0 +1,93 @@
+/**
+ * List of elements available to be added to quiz form.
+ *
+ * Last used ID: 16.
+ */
+function Core_Quiz_Element_ElementSid()
+{
+  // Empty constructor.
+}
+
+/**
+ * Question element with single item select via dropdown.
+ *
+ * @type {number}
+ */
+Core_Quiz_Element_ElementSid.CHECKBOX=15;
+
+/**
+ * Style element which represents single line divide.
+ *
+ * @type {number}
+ */
+Core_Quiz_Element_ElementSid.DIVIDE=2;
+
+/**
+ * Question element with single item select via dropdown.
+ *
+ * @type {number}
+ */
+Core_Quiz_Element_ElementSid.DROPDOWN=5;
+
+/**
+ * Question element to specify image markup.
+ *
+ * @type {number}
+ */
+Core_Quiz_Element_ElementSid.IMAGE_MARKUP=16;
+
+/**
+ * Question element to specify initials signature.
+ *
+ * @type {number}
+ */
+Core_Quiz_Element_ElementSid.INITIALS=13;
+
+/**
+ * Question element with single line answer.
+ *
+ * @type {number}
+ */
+Core_Quiz_Element_ElementSid.INPUT=1;
+
+/**
+ * Style element which represents page break.
+ *
+ * @type {number}
+ */
+Core_Quiz_Element_ElementSid.PAGE_BREAK=3;
+
+/**
+ * Question element with single item select via radio buttons.
+ *
+ * @type {number}
+ */
+Core_Quiz_Element_ElementSid.RADIO=6;
+
+/**
+ * Question element to select rate.
+ *
+ * @type {number}
+ */
+Core_Quiz_Element_ElementSid.RATE=12;
+
+/**
+ * Question element allows to specify signature.
+ *
+ * @type {number}
+ */
+Core_Quiz_Element_ElementSid.SIGNATURE=11;
+
+/**
+ * Style element to add custom text string.
+ *
+ * @type {number}
+ */
+Core_Quiz_Element_ElementSid.TEXT=10;
+
+/**
+ * Question element with multiple line answer.
+ *
+ * @type {number}
+ */
+Core_Quiz_Element_ElementSid.TEXTAREA=4;
