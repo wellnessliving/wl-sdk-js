93541bfe0b8eef4902d44eb02d4ca7a2bc7b104f
ihor.shein <ihorshein@gmail.com>
2024-09-27 22:13:16 +0200

    wl-64811: [JS-SDK] Store prizes; Quick gift cards +review WL-56120

--- WellnessLiving/Wl/Catalog/Cart/CartModel.js
+++ WellnessLiving/Wl/Catalog/Cart/CartModel.js
@@ -1,5 +1,5 @@
 /**
- * Information about the cart in the catalog.
+ * Displays information about the cart in the catalog.
  *
  * This model is generated automatically based on API.
  *
@@ -16,10 +16,9 @@ function Wl_Catalog_Cart_CartModel()
   this._s_key = "k_location,uid";
 
   /**
-   * List of discount for each item.
+   * The list of discounts for each item.
    *
-   * Key is unique key of item.
-   * Value is discount amount for this item.
+   * Keys refer to unique item keys, and values refer to discount amounts for the item.
    *
    * @get result
    * @type {string[]}
@@ -28,39 +27,146 @@ function Wl_Catalog_Cart_CartModel()
 
   /**
    * @typedef {{}} Wl_Catalog_Cart_CartModel_a_item
-   * @property {number} i_quantity Quantity of sale items.
-   * @property {number} id_sale Sale item type, one of {@link RsSaleSid}.
-   * @property {string} k_id Sale item key.
-   * @property {string} k_shop_product_option Shop product option. <tt>null</tt> if sale item has no options.
+   * @property {string} dl_client_prorate The client prorate date. This will be `null` in cases where the client prorate date hasn't passed.
+   * @property {number} i_quantity The quantity of sale items.
+   * @property {number} id_sale The sale item type, one of the {@link RsSaleSid} constants.
+   * @property {string} k_id The sale item key.
+   * @property {*} k_login_prize Key of login prize used on item, primary key in {@link \RsLoginPrizeSql} table.
+   * @property {string} k_shop_product_option The shop product option. If empty string, the sale item has no options.
    */
 
   /**
-   * A list of items in the cart.
+   * The list of items in the cart.
    * <dl>
+   *   <dt>string <var>dl_client_prorate</var></dt>
+   *   <dd>The client prorate date. This will be `null` in cases where the client prorate date hasn't passed.</dd>
    *   <dt>int <var>i_quantity</var></dt>
-   *   <dd>Quantity of sale items.</dd>
+   *   <dd>The quantity of sale items.</dd>
    *   <dt>int <var>id_sale</var></dt>
-   *   <dd>Sale item type, one of {@link RsSaleSid}.</dd>
+   *   <dd>The sale item type, one of the {@link RsSaleSid} constants.</dd>
    *   <dt>string <var>k_id</var></dt>
-   *   <dd>Sale item key.</dd>
+   *   <dd>The sale item key.</dd>
+   *   <dt>?string [<var>k_login_prize</var>]</dt>
+   *   <dd>Key of login prize used on item, primary key in {@link \RsLoginPrizeSql} table.</dd>
    *   <dt>string <var>k_shop_product_option</var></dt>
-   *   <dd>Shop product option. <tt>null</tt> if sale item has no options.</dd>
+   *   <dd>The shop product option. If empty string, the sale item has no options.</dd>
    * </dl>
    *
-   * @get get
+   * @get get,result
    * @type {Wl_Catalog_Cart_CartModel_a_item[]}
    */
   this.a_item = [];
 
   /**
+   * @typedef {{}} Wl_Catalog_Cart_CartModel_a_prize_propose
+   * @property {number} i_score Amount of points needed to redeem the prize.
+   * @property {string} k_reward_prize Reward prize key, primary key in {@link \RsRewardPrizeSql} table.
+   * @property {string} text_discount Reward prize discount, e.g. '$10.00 off', '10% off'.
+   * @property {string} text_title Reward prize title, e.g. 'on any item', 'on Water bottle', 'on any Appointment Pass or Membership Purchase Option'.
+   */
+
+  /**
+   * List of prizes that can be redeemed and applied to items in the cart.
+   * <dl>
+   *   <dt>int <var>i_score</var></dt>
+   *   <dd>Amount of points needed to redeem the prize.</dd>
+   *   <dt>string <var>k_reward_prize</var></dt>
+   *   <dd>Reward prize key, primary key in {@link \RsRewardPrizeSql} table.</dd>
+   *   <dt>string <var>text_discount</var></dt>
+   *   <dd>Reward prize discount, e.g. '$10.00 off', '10% off'.</dd>
+   *   <dt>string <var>text_title</var></dt>
+   *   <dd>Reward prize title, e.g. 'on any item', 'on Water bottle', 'on any Appointment Pass or Membership Purchase Option'.</dd>
+   * </dl>
+   *
+   * @get result
+   * @type {Wl_Catalog_Cart_CartModel_a_prize_propose[]}
+   */
+  this.a_prize_propose = [];
+
+  /**
+   * @typedef {{}} Wl_Catalog_Cart_CartModel_a_reward_item
+   * @property {number} i_quantity The quantity of sale items.
+   * @property {number} id_sale The sale item type, one of the {@link RsSaleSid} constants.
+   * @property {boolean} is_login_prize_used Whether selected login prize discount applied to item.
+   * @property {string} k_id The sale item key.
+   * @property {string} k_shop_product_option The shop product option. If <tt>null</tt>, the sale item has no options.
+   */
+
+  /**
+   * List of cart items to which the selected prize can be applied
+   * <dl>
+   *   <dt>int <var>i_quantity</var></dt>
+   *   <dd>The quantity of sale items.</dd>
+   *   <dt>int <var>id_sale</var></dt>
+   *   <dd>The sale item type, one of the {@link RsSaleSid} constants.</dd>
+   *   <dt>bool <var>is_login_prize_used</var></dt>
+   *   <dd>Whether selected login prize discount applied to item.</dd>
+   *   <dt>string <var>k_id</var></dt>
+   *   <dd>The sale item key.</dd>
+   *   <dt>string <var>k_shop_product_option</var></dt>
+   *   <dd>The shop product option. If <tt>null</tt>, the sale item has no options.</dd>
+   * </dl>
+   *
+   * @get result
+   * @type {Wl_Catalog_Cart_CartModel_a_reward_item[]}
+   */
+  this.a_reward_item = [];
+
+  /**
+   * @typedef {{}} Wl_Catalog_Cart_CartModel_a_reward_propose
+   * @property {string} k_login_prize Login prize key, primary key in {@link \RsLoginPrizeSql} table.
+   * @property {string} text_discount Reward prize discount, e.g. '$10.00 off', '10% off'.
+   * @property {string} text_discount_applied Applied login prize discount title, e.g. '10% discount'.
+   * @property {string} text_discount_purpose Purpose login prize discount title, e.g. 'Apply 10% off'.
+   * @property {string} text_title Reward prize title, e.g. 'on any item', 'on Water bottle', 'on any Appointment Pass or Membership Purchase Option'.
+   */
+
+  /**
+   * List of login prizes that can be applied to items in the cart.
+   * <dl>
+   *   <dt>string <var>k_login_prize</var></dt>
+   *   <dd>Login prize key, primary key in {@link \RsLoginPrizeSql} table.</dd>
+   *   <dt>string <var>text_discount</var></dt>
+   *   <dd>Reward prize discount, e.g. '$10.00 off', '10% off'.</dd>
+   *   <dt>string <var>text_discount_applied</var></dt>
+   *   <dd>Applied login prize discount title, e.g. '10% discount'.</dd>
+   *   <dt>string <var>text_discount_purpose</var></dt>
+   *   <dd>Purpose login prize discount title, e.g. 'Apply 10% off'.</dd>
+   *   <dt>string <var>text_title</var></dt>
+   *   <dd>Reward prize title, e.g. 'on any item', 'on Water bottle', 'on any Appointment Pass or Membership Purchase Option'.</dd>
+   * </dl>
+   *
+   * @get result
+   * @type {Wl_Catalog_Cart_CartModel_a_reward_propose[]}
+   */
+  this.a_reward_propose = [];
+
+  /**
    * Values derived for individual tax rates.
-   * Key is key of the tax (primary key in {@link \RsTaxSql}).
-   * Value is the amount of tax in cart.
+   *
+   * Keys refer to tax keys.
+   * And values refer to the amount of tax in the cart.
    *
    * @get result
    * @type {{}}
    */
   this.a_tax_list = [];
+
+  /**
+   * Amount of client's reward points.
+   *
+   * @get result
+   * @type {?number}
+   */
+  this.i_score = null;
+
+  /**
+   * Whether selected login prize should be auto applied to first applicable item.
+   *
+   * @get get
+   * @type {boolean}
+   */
+  this.is_auto_apply_prize = true;
 
   /**
    * The location key.
@@ -71,9 +177,17 @@ function Wl_Catalog_Cart_CartModel()
   this.k_location = "0";
 
   /**
+   * The login prize key.
+   *
+   * @get get
+   * @type {string}
+   */
+  this.k_login_prize = "0";
+
+  /**
    * The full discount of the cart.
    *
-   * <tt>null</tt> if not calculated yet.
+   * If `null`, the discount isn't calculated yet.
    *
    * @get result
    * @type {?string}
@@ -81,7 +195,7 @@ function Wl_Catalog_Cart_CartModel()
   this.m_discount = null;
 
   /**
-   * Amount of total discount.
+   * The total discount amount.
    *
    * @get result
    * @type {string}
@@ -91,7 +205,7 @@ function Wl_Catalog_Cart_CartModel()
   /**
    * The total amount in the catalog cart without tax.
    *
-   * <tt>null</tt> if not set yet.
+   * If `null`, the total amount isn't set yet.
    *
    * @get result
    * @type {?string}
@@ -99,9 +213,9 @@ function Wl_Catalog_Cart_CartModel()
   this.m_subtotal = null;
 
   /**
-   * The amount of tax.
+   * The tax amount.
    *
-   * <tt>null</tt> if not set yet.
+   * If `null`, the tax amount isn't set yet.
    *
    * @get result
    * @type {?string}
@@ -109,7 +223,7 @@ function Wl_Catalog_Cart_CartModel()
   this.m_tax = null;
 
   /**
-   * Amount of appointment tips.
+   * The amount of appointment tips.
    *
    * @get result
    * @type {?string}
@@ -119,7 +233,7 @@ function Wl_Catalog_Cart_CartModel()
   /**
    * The total amount in the catalog cart.
    *
-   * <tt>null</tt> if not set yet.
+   * If `null`, the total amount isn't set yet.
    *
    * @get result
    * @type {?string}
@@ -129,7 +243,7 @@ function Wl_Catalog_Cart_CartModel()
   /**
    * The discount code.
    *
-   * <tt>null</tt> means that the discount code has been reset.
+   * If `null`, the discount code has been reset.
    *
    * @get get
    * @type {?string}
@@ -139,7 +253,7 @@ function Wl_Catalog_Cart_CartModel()
   /**
    * The user key.
    *
-   * `0` in case when guest.
+   * This will be `0` for guests.
    *
    * @get get
    * @type {string}
@@ -156,14 +270,14 @@ WlSdk_ModelAbstract.extend(Wl_Catalog_Cart_CartModel);
  */
 Wl_Catalog_Cart_CartModel.prototype.config=function()
 {
-  return {"a_field": {"a_discount_item": {"get": {"result": true}},"a_item": {"get": {"get": true}},"a_tax_list": {"get": {"result": true}},"k_location": {"get": {"get": true}},"m_discount": {"get": {"result": true}},"m_discount_total": {"get": {"result": true}},"m_subtotal": {"get": {"result": true}},"m_tax": {"get": {"result": true}},"m_tip_purchase": {"get": {"result": true}},"m_total": {"get": {"result": true}},"text_discount_code": {"get": {"get": true}},"uid": {"get": {"get": true}}}};
+  return {"a_field": {"a_discount_item": {"get": {"result": true}},"a_item": {"get": {"get": true,"result": true}},"a_prize_propose": {"get": {"result": true}},"a_reward_item": {"get": {"result": true}},"a_reward_propose": {"get": {"result": true}},"a_tax_list": {"get": {"result": true}},"i_score": {"get": {"result": true}},"is_auto_apply_prize": {"get": {"get": true}},"k_location": {"get": {"get": true}},"k_login_prize": {"get": {"get": true}},"m_discount": {"get": {"result": true}},"m_discount_total": {"get": {"result": true}},"m_subtotal": {"get": {"result": true}},"m_tax": {"get": {"result": true}},"m_tip_purchase": {"get": {"result": true}},"m_total": {"get": {"result": true}},"text_discount_code": {"get": {"get": true}},"uid": {"get": {"get": true}}}};
 };
 
 /**
  * @function
  * @name Wl_Catalog_Cart_CartModel.instanceGet
  * @param {string} k_location The location key.
- * @param {string} uid The user key. `0` in case when guest.
+ * @param {string} uid The user key. This will be `0` for guests.
  * @returns {Wl_Catalog_Cart_CartModel}
  * @see WlSdk_ModelAbstract.instanceGet()
  */
