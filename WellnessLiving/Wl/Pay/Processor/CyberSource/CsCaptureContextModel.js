cec36e8cab33ca9d96e95db9fc3d6f8f001dd810
Proffesor <andrii.m@wellnessliving.com>
2025-03-11 09:51:11 +0200

    wl-69841: Wl_Pay_Processor_CyberSource_CsCaptureContextModel - integrity access +review WL-58899

--- WellnessLiving/Wl/Pay/Processor/CyberSource/CsCaptureContextModel.js
+++ WellnessLiving/Wl/Pay/Processor/CyberSource/CsCaptureContextModel.js
@@ -27,6 +27,21 @@ function Wl_Pay_Processor_CyberSource_CsCaptureContextModel()
   this.k_business_merchant = "";
 
   /**
+   * Value to improve security for payment card transactions, safeguarding sensitive information, maintaining trust in
+   *  electronic payment systems, and reducing the likelihood of a data breach.
+   *
+   * See {@link https://support.visaacceptance.com/knowledgebase/Knowledgearticle/?code=KA-07551} for more details.
+   * Uses to create JS script tag together with {@link Wl_Pay_Processor_CyberSource_CsCaptureContextModel.url_client_library}.
+   *
+   *
+   * `null` until generated.
+   *
+   * @post result
+   * @type {?string}
+   */
+  this.s_client_library_integrity = null;
+
+  /**
    * Contents of the key.
    *
    * `null` until generated.
@@ -48,6 +63,7 @@ function Wl_Pay_Processor_CyberSource_CsCaptureContextModel()
 
   /**
    * Microform Integration JavaScript library url.
+   * Uses to create JS script tag together with {@link Wl_Pay_Processor_CyberSource_CsCaptureContextModel.s_client_library_integrity}.
    *
    * `null` until generated.
    *
@@ -66,5 +82,5 @@ WlSdk_ModelAbstract.extend(Wl_Pay_Processor_CyberSource_CsCaptureContextModel);
  */
 Wl_Pay_Processor_CyberSource_CsCaptureContextModel.prototype.config=function()
 {
-  return {"a_field": {"k_business": {"post": {"post": true}},"k_business_merchant": {"post": {"post": true}},"s_key": {"post": {"result": true}},"s_key_id": {"post": {"result": true}},"url_client_library": {"post": {"result": true}}}};
+  return {"a_field": {"k_business": {"post": {"post": true}},"k_business_merchant": {"post": {"post": true}},"s_client_library_integrity": {"post": {"result": true}},"s_key": {"post": {"result": true}},"s_key_id": {"post": {"result": true}},"url_client_library": {"post": {"result": true}}}};
 };
