075fa81848f269c8f1923eb17a2191ad001b382c
GordeyUA <gordievsky@gmail.com>
2025-06-18 14:50:19 +0300

    wl-72405: Guest Booking Flow Detection +review WL-60272

--- WellnessLiving/Wl/Profile/Edit/Email/EditEmailModel.js
+++ WellnessLiving/Wl/Profile/Edit/Email/EditEmailModel.js
@@ -15,7 +15,7 @@ function Wl_Profile_Edit_Email_EditEmailModel()
   /**
    * @inheritDoc
    */
-  this._s_key = "k_business,text_mail,uid_want";
+  this._s_key = "is_backend,k_business,text_mail,uid_want";
 
   /**
    * @typedef {{}} Wl_Profile_Edit_Email_EditEmailModel_a_user
@@ -36,33 +36,6 @@ function Wl_Profile_Edit_Email_EditEmailModel()
   /**
    * Information about the user who occupies the specified email.
    * This will be empty if the email is free or if the rate limit has been reached.
-   * Otherwise, has next keys:
-   * <dl>
-   *   <dt>
-   *     string <var>text_firstname</var>
-   *   </dt>
-   *   <dd>
-   *     First name.
-   *   </dd>
-   *   <dt>
-   *     string <var>text_lastname</var>
-   *   </dt>
-   *   <dd>
-   *     Last name.
-   *   </dd>
-   *   <dt>
-   *     string <var>text_phone</var>
-   *   </dt>
-   *   <dd>
-   *     Phone.
-   *   </dd>
-   *   <dt>
-   *     string <var>url_photo</var>
-   *   </dt>
-   *   <dd>
-   *     URL to photo.
-   *   </dd>
-   * </dl>
    *
    * @get result
    * @type {Wl_Profile_Edit_Email_EditEmailModel_a_user}
@@ -70,13 +43,21 @@ function Wl_Profile_Edit_Email_EditEmailModel()
   this.a_user = undefined;
 
   /**
-   * Shows, whether client was registered in the business: <tt>true</tt> if user was added to the business,
-   * <tt>false</tt> if staff only received temporary access, because mandatory fields must be specified first.
+   * Shows, whether client was registered in the business: `true` if user was added to the business,
+   * `false` if staff only received temporary access, because mandatory fields must be specified first.
    *
    * @get result
    * @type {boolean}
    */
   this.is_added = false;
+
+  /**
+   * If `true`, the API is being used from backend. Otherwise, this will be `false`.
+   *
+   * @get get
+   * @type {boolean}
+   */
+  this.is_backend = true;
 
   /**
    * If `true`, user is already a member of current business, `false` - otherwise.
@@ -95,12 +76,28 @@ function Wl_Profile_Edit_Email_EditEmailModel()
   this.is_limit = undefined;
 
   /**
-   * If `true`, user, who has the specified email, is staff, `false` - otherwise.
+   * If `true`, user, who has the specified email, has password set, `false` - otherwise.
+   *
+   * @get result
+   * @type {boolean}
+   */
+  this.is_password_set = false;
+
+  /**
+   * If `true`, user, who has the specified email, is staff in current business, `false` - otherwise.
    *
    * @get result
    * @type {boolean}
    */
   this.is_staff = false;
+
+  /**
+   * If `true`, user, who has the specified email, is staff in any business, `false` - otherwise.
+   *
+   * @get result
+   * @type {boolean}
+   */
+  this.is_staff_any_business = false;
 
   /**
    * If `true`, then the specified email is in use. Otherwise, this will be `false`.
@@ -156,12 +153,13 @@ WlSdk_ModelAbstract.extend(Wl_Profile_Edit_Email_EditEmailModel);
  */
 Wl_Profile_Edit_Email_EditEmailModel.prototype.config=function()
 {
-  return {"a_field": {"a_business_member_key": {"get": {"result": true}},"a_user": {"get": {"result": true}},"is_added": {"get": {"result": true}},"is_current_member": {"get": {"result": true}},"is_limit": {"get": {"result": true}},"is_staff": {"get": {"result": true}},"is_use": {"get": {"result": true}},"k_business": {"get": {"get": true},"post": {"get": true}},"text_mail": {"get": {"get": true}},"uid_result": {"get": {"result": true}},"uid_want": {"get": {"get": true},"post": {"get": true}}}};
+  return {"a_field": {"a_business_member_key": {"get": {"result": true}},"a_user": {"get": {"result": true}},"is_added": {"get": {"result": true}},"is_backend": {"get": {"get": true}},"is_current_member": {"get": {"result": true}},"is_limit": {"get": {"result": true}},"is_password_set": {"get": {"result": true}},"is_staff": {"get": {"result": true}},"is_staff_any_business": {"get": {"result": true}},"is_use": {"get": {"result": true}},"k_business": {"get": {"get": true},"post": {"get": true}},"text_mail": {"get": {"get": true}},"uid_result": {"get": {"result": true}},"uid_want": {"get": {"get": true},"post": {"get": true}}}};
 };
 
 /**
  * @function
  * @name Wl_Profile_Edit_Email_EditEmailModel.instanceGet
+ * @param {boolean} is_backend Whether API is being used from backend.
  * @param {string} k_business The business key where the check must be performed.
  * @param {string} text_mail The email address to be checked.
  * @param {string} uid_want The key of user whose email will be edited. This will be `0` in the case of a new user creation.
