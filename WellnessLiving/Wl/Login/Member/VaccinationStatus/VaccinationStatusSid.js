abb38aa193ae39a6995c9f0b5730640eecd7ed35
bulletof <bulletof@gmail.com>
2025-09-17 09:34:39 +0300

    wl-74992: Standard Lead Capture API for AI Agent

---
+++ WellnessLiving/Wl/Login/Member/VaccinationStatus/VaccinationStatusSid.js
@@ -0,0 +1,35 @@
+/**
+ * Class to work with vaccination status string identifiers.
+ */
+function Wl_Login_Member_VaccinationStatus_VaccinationStatusSid()
+{
+  // Empty constructor.
+}
+
+/**
+ * Fully vaccinated.
+ *
+ * @type {number}
+ */
+Wl_Login_Member_VaccinationStatus_VaccinationStatusSid.FULL=3;
+
+/**
+ * Not vaccinated
+ *
+ * @type {number}
+ */
+Wl_Login_Member_VaccinationStatus_VaccinationStatusSid.NONE=1;
+
+/**
+ * Partially vaccinated.
+ *
+ * @type {number}
+ */
+Wl_Login_Member_VaccinationStatus_VaccinationStatusSid.PARTIAL=2;
+
+/**
+ * Vaccination status is undefined.
+ *
+ * @type {number}
+ */
+Wl_Login_Member_VaccinationStatus_VaccinationStatusSid.UNKNOWN=2;
--- WellnessLiving/Wl/Profile/ProfileCreateModel.js
+++ WellnessLiving/Wl/Profile/ProfileCreateModel.js
@@ -19,12 +19,72 @@ function Wl_Profile_ProfileCreateModel()
   this.dt_birthday = "";
 
   /**
+   * Gender ID.
+   * One of the {@link Wl_Gender_GenderSid} constants.
+   *
+   * `0` if not specified.
+   *
+   * @post post
+   * @type {number}
+   */
+  this.id_gender = 0;
+
+  /**
+   * Lead source ID.
+   *
+   * One of the {@link Wl_Mode_ModeSid} constants.
+   * `0` if not specified.
+   *
+   * @post post
+   * @type {number}
+   */
+  this.id_lead_source = 0;
+
+  /**
+   * Vaccination status ID.
+   *
+   * One of the {@link Wl_Login_Member_VaccinationStatus_VaccinationStatusSid} constants.
+   *
+   * `0` if not specified.
+   *
+   * @post post
+   * @type {number}
+   */
+  this.id_vaccination_status = 0;
+
+  /**
    * The key of the business.
    *
    * @post post
    * @type {string}
    */
   this.k_business = "";
+
+  /**
+   * The key of the lead source.
+   *
+   * Empty string if not specified.
+   *
+   * @post post
+   * @type {string}
+   */
+  this.k_lead_source = "";
+
+  /**
+   * The key of the home location.
+   *
+   * @post post
+   * @type {string}
+   */
+  this.k_location_home = "";
+
+  /**
+   * The address of the user.
+   *
+   * @post post
+   * @type {string}
+   */
+  this.text_address = "";
 
   /**
    * First name of the user.
@@ -59,12 +119,38 @@ function Wl_Profile_ProfileCreateModel()
   this.text_phone = "";
 
   /**
+   * Home phone of the user.
+   *
+   * @post post
+   * @type {string}
+   */
+  this.text_phone_home = "";
+
+  /**
+   * Work phone of the user.
+   *
+   * @post post
+   * @type {string}
+   */
+  this.text_phone_work = "";
+
+  /**
    * The key of the user.
    *
    * @post result
    * @type {string}
    */
   this.uid = "";
+
+  /**
+   * Referrer user key.
+   *
+   * Empty string if not specified.
+   *
+   * @post post
+   * @type {string}
+   */
+  this.uid_referrer = "";
 
   this.changeInit();
 }
@@ -76,5 +162,5 @@ WlSdk_ModelAbstract.extend(Wl_Profile_ProfileCreateModel);
  */
 Wl_Profile_ProfileCreateModel.prototype.config=function()
 {
-  return {"a_field": {"dt_birthday": {"post": {"post": true}},"k_business": {"post": {"post": true}},"text_firstname": {"post": {"post": true}},"text_lastname": {"post": {"post": true}},"text_mail": {"post": {"post": true}},"text_phone": {"post": {"post": true}},"uid": {"post": {"result": true}}}};
+  return {"a_field": {"dt_birthday": {"post": {"post": true}},"id_gender": {"post": {"post": true}},"id_lead_source": {"post": {"post": true}},"id_vaccination_status": {"post": {"post": true}},"k_business": {"post": {"post": true}},"k_lead_source": {"post": {"post": true}},"k_location_home": {"post": {"post": true}},"text_address": {"post": {"post": true}},"text_firstname": {"post": {"post": true}},"text_lastname": {"post": {"post": true}},"text_mail": {"post": {"post": true}},"text_phone": {"post": {"post": true}},"text_phone_home": {"post": {"post": true}},"text_phone_work": {"post": {"post": true}},"uid": {"post": {"result": true}},"uid_referrer": {"post": {"post": true}}}};
 };
