eb5ea9e709c15c5e1f202aa1af283435730165a9
Vladislav Kobzev <kp42@ya.ru>
2025-09-24 15:14:02 +0300

    wl-75099: Add "CAASI AI" as Lead Source for AI Agent Generated Leads +review WL-61581

--- WellnessLiving/Wl/Mode/ModeSid.js
+++ WellnessLiving/Wl/Mode/ModeSid.js
@@ -9,32 +9,95 @@ function Wl_Mode_ModeSid()
 }
 
 /**
- * Visit has been created by ClassPass.
+ * Action made via Api Endpoint. Default for leads created via API, unless overridden.
  *
  * @type {number}
  */
-Wl_Mode_ModeSid.CLASSPASS_BOOKING = 8;
+Wl_Mode_ModeSid.API=28;
 
 /**
- * Visit has been crated by Google Booking Service.
+ * Registered through <tt>Azure</tt>.
  *
  * @type {number}
  */
-Wl_Mode_ModeSid.GOOGLE_BOOKING = 7;
+Wl_Mode_ModeSid.AZURE=21;
 
 /**
- * Visit has been crated by <tt>GymPass</tt>.
+ * Visit has been created by <tt>CENTRED</tt>.
  *
  * @type {number}
  */
-Wl_Mode_ModeSid.GYMPASS_BOOKING = 14;
+Wl_Mode_ModeSid.CENTRED=23;
+
+/**
+ * Visit has been created by <tt>ClassPass</tt>.
+ *
+ * @type {number}
+ */
+Wl_Mode_ModeSid.CLASSPASS_BOOKING=8;
+
+/**
+ * Debt paid via collections.
+ *
+ * @type {number}
+ */
+Wl_Mode_ModeSid.COLLECTIONS=22;
+
+/**
+ * Debt paid via collections.
+ *
+ * @type {number}
+ */
+Wl_Mode_ModeSid.COLLECTIONS_FUTURE=26;
+
+/**
+ * Action from Concerto.
+ *
+ * @type {number}
+ */
+Wl_Mode_ModeSid.CONCERTO=27;
+
+/**
+ * Action made via email.
+ *
+ * @type {number}
+ */
+Wl_Mode_ModeSid.EMAIL=18;
+
+/**
+ * Indicating that the source is Facebook.
+ *
+ * @type {number}
+ */
+Wl_Mode_ModeSid.FACEBOOK=20;
+
+/**
+ * Indicating that the source is Google.
+ *
+ * @type {number}
+ */
+Wl_Mode_ModeSid.GOOGLE=19;
+
+/**
+ * Visit has been created by Google Booking Service.
+ *
+ * @type {number}
+ */
+Wl_Mode_ModeSid.GOOGLE_BOOKING=7;
+
+/**
+ * Visit has been created by <tt>GymPass</tt>.
+ *
+ * @type {number}
+ */
+Wl_Mode_ModeSid.GYMPASS_BOOKING=14;
 
 /**
  * Visit was created during import.
  *
  * @type {number}
  */
-Wl_Mode_ModeSid.IMPORT = 5;
+Wl_Mode_ModeSid.IMPORT=5;
 
 /**
  * Action made via microsite.
@@ -43,74 +106,95 @@ Wl_Mode_ModeSid.IMPORT = 5;
  *
  * @type {number}
  */
-Wl_Mode_ModeSid.MICROSITE = 12;
+Wl_Mode_ModeSid.MICROSITE=12;
+
+/**
+ * Indicating that the source is Microsoft.
+ *
+ * @type {number}
+ */
+Wl_Mode_ModeSid.MICROSOFT=24;
 
 /**
  * Client booked session on My Presence Site.
  *
  * @type {number}
  */
-Wl_Mode_ModeSid.MY_PRESENCE_SITE = 13;
+Wl_Mode_ModeSid.MY_PRESENCE_SITE=13;
+
+/**
+ * Action made via SMS.
+ *
+ * @type {number}
+ */
+Wl_Mode_ModeSid.SMS=17;
 
 /**
  * Staff booked session from spa backend.
  *
  * @type {number}
  */
-Wl_Mode_ModeSid.SPA_BACKEND = 4;
+Wl_Mode_ModeSid.SPA_BACKEND=4;
 
 /**
  * Client booked session from spa frontend.
  *
  * @type {number}
  */
-Wl_Mode_ModeSid.SPA_FRONTEND = 3;
+Wl_Mode_ModeSid.SPA_FRONTEND=3;
 
 /**
  * Created by system.
  *
  * @type {number}
  */
-Wl_Mode_ModeSid.SYSTEM = 10;
+Wl_Mode_ModeSid.SYSTEM=10;
 
 /**
  * Means that we did not define mode.
  *
  * @type {number}
  */
-Wl_Mode_ModeSid.UNDEFINED = 6;
+Wl_Mode_ModeSid.UNDEFINED=6;
 
 /**
  * Client booked session from Attendance Web App.
  *
  * @type {number}
  */
-Wl_Mode_ModeSid.WEB_APP_ATTENDANCE = 16;
+Wl_Mode_ModeSid.WEB_APP_ATTENDANCE=16;
 
 /**
  * Client checked-in for the session through Check-In Web App.
  *
  * @type {number}
  */
-Wl_Mode_ModeSid.WEB_APP_CHECK_IN = 15;
+Wl_Mode_ModeSid.WEB_APP_CHECK_IN=15;
 
 /**
  * Staff booked session for client from website backend.
  *
  * @type {number}
  */
-Wl_Mode_ModeSid.WEB_BACKEND = 2;
+Wl_Mode_ModeSid.WEB_BACKEND=2;
 
 /**
  * Client booked session from website frontend.
  *
  * @type {number}
  */
-Wl_Mode_ModeSid.WEB_FRONTEND = 1;
+Wl_Mode_ModeSid.WEB_FRONTEND=1;
 
 /**
  * Action made via widget (purchase, book etc).
  *
  * @type {number}
  */
-Wl_Mode_ModeSid.WIDGET = 11;
+Wl_Mode_ModeSid.WIDGET=11;
+
+/**
+ * Action from Zapier.
+ *
+ * @type {number}
+ */
+Wl_Mode_ModeSid.ZAPIER=25;
