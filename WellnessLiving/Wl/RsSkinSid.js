ccdf1a671e7c965fb21df51107bdd3343f77ee41
BuildTools <unconfigured@null.spigotmc.org>
2025-11-27 10:53:30 +0200

    wl-76527: [Presence] API JS SDK + PHP SDK: Model for getting list of widgets and their skins +review WL-62363

---
+++ WellnessLiving/Wl/RsSkinSid.js
@@ -0,0 +1,115 @@
+/**
+ * Possible ways to stop repeatable events.
+ */
+function RsSkinSid()
+{
+  // Empty constructor.
+}
+
+/**
+ * Ai Agent web-chat.
+ *
+ * @type {number}
+ */
+RsSkinSid.AI_AGENT=19;
+
+/**
+ * Application skin.
+ *
+ * @type {number}
+ */
+RsSkinSid.APPLICATION=13;
+
+/**
+ * Appointment booking wizard.
+ *
+ * @type {number}
+ */
+RsSkinSid.APPOINTMENT=7;
+
+/**
+ * Event enrollment wizard widget.
+ *
+ * @type {number}
+ */
+RsSkinSid.ENROLLMENT=8;
+
+/**
+ * Fitbuilder widget.
+ *
+ * @type {number}
+ */
+RsSkinSid.FITBUILDER=17;
+
+/**
+ * Skin for lead tracking form.
+ *
+ * @type {number}
+ */
+RsSkinSid.LEAD=9;
+
+/**
+ * Mobile schedule version.
+ *
+ * @type {number}
+ */
+RsSkinSid.MOBILE_SCHEDULE=10;
+
+/**
+ * Asset booking wizard.
+ *
+ * @type {number}
+ */
+RsSkinSid.RESOURCE=16;
+
+/**
+ * Review list widget.
+ *
+ * @type {number}
+ */
+RsSkinSid.REVIEW_LIST=4;
+
+/**
+ * General schedule design appearance.
+ *
+ * @type {number}
+ */
+RsSkinSid.SCHEDULE_DESIGN=15;
+
+/**
+ * Foreign schedule widget on react language.
+ *
+ * @type {number}
+ */
+RsSkinSid.SCHEDULE_FOREIGN=18;
+
+/**
+ * Schedule list widget.
+ *
+ * @type {number}
+ */
+RsSkinSid.SCHEDULE_LIST=1;
+
+/**
+ * Standard version of schedule.
+ *
+ * This skin is the same as {@link RsSkinSid::SCHEDULE_LIST} except that design can not be changed, only standard
+ * settings can be used.
+ *
+ * @type {number}
+ */
+RsSkinSid.SCHEDULE_STANDARD=14;
+
+/**
+ * Staff widget.
+ *
+ * @type {number}
+ */
+RsSkinSid.STAFF=12;
+
+/**
+ * Online store widget.
+ *
+ * @type {number}
+ */
+RsSkinSid.STORE=11;
---
+++ WellnessLiving/Wl/Skin/Widget/SkinWidgetListModel.js
@@ -0,0 +1,56 @@
+/**
+ * Lists widget skins grouped by widget type for a business.
+ *
+ * This model is generated automatically based on API.
+ *
+ * @augments WlSdk_ModelAbstract
+ * @constructor
+ */
+function Wl_Skin_Widget_SkinWidgetListModel()
+{
+  WlSdk_ModelAbstract.apply(this);
+
+  /**
+   * @typedef {[]} Wl_Skin_Widget_SkinWidgetListModel_a_widget_skin
+   * @property {string} k_skin Skin key.
+   * @property {string} text_widget_name Widget name.
+   */
+
+  /**
+   * List of Widget skins grouped by widget type.
+   *
+   * Skin type, one of {@link RsSkinSid} constants.
+   *
+   * The array structure:
+   * Keys - Skin type, one of {@link RsSkinSid} constants.
+   * Values - arrays with next keys:
+   * <dl>
+   *   <dt>string `k_skin`</dt><dd>Skin key.</dd>
+   *   <dt>string `text_widget_name`</dt><dd>Widget name.</dd>
+   * </dl>
+   *
+   * @get result
+   * @type {Wl_Skin_Widget_SkinWidgetListModel_a_widget_skin[]}
+   */
+  this.a_widget_skin = [];
+
+  /**
+   * Business key.
+   *
+   * @get get
+   * @type {string}
+   */
+  this.k_business = "";
+
+  this.changeInit();
+}
+
+WlSdk_ModelAbstract.extend(Wl_Skin_Widget_SkinWidgetListModel);
+
+/**
+ * @inheritDoc
+ */
+Wl_Skin_Widget_SkinWidgetListModel.prototype.config=function()
+{
+  return {"a_field": {"a_widget_skin": {"get": {"result": true}},"k_business": {"get": {"get": true}}}};
+};
