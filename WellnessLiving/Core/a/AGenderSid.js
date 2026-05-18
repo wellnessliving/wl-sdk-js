099f19502395f67a1739dc757f99b425f2fca683
GordeyUA <gordievsky@gmail.com>
2026-05-18 15:39:52 +0300

    wl-80800: API: Build next-available-date endpoint for Appointments +review WL-64605

---
+++ WellnessLiving/Core/a/AGenderSid.js
@@ -0,0 +1,22 @@
+/**
+ * String identifiers for gender.
+ */
+function AGenderSid()
+{
+  // Empty constructor.
+}
+
+/**
+ * Female gender.
+ */
+AGenderSid.FEMALE = 2;
+
+/**
+ * Male gender.
+ */
+AGenderSid.MALE = 1;
+
+/**
+ * Gender is undefined in cases where the user preferred not to identify their gender.
+ */
+AGenderSid.UNDEFINED = 3;
