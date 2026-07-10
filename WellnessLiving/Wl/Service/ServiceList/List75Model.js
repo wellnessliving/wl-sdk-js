5cc23187bea7859034550aae068a7f48c65dbbf5
sergiy <sidose4@gmail.com>
2026-07-10 13:50:24 +0300

    wl-82358: Fix filtering by Event Type and Instance to all Marketing Suite service selectors +review WL-65345

--- WellnessLiving/Wl/Service/ServiceList/List75Model.js
+++ WellnessLiving/Wl/Service/ServiceList/List75Model.js
@@ -1,11 +1,8 @@
 /**
  * Versioned list of appointment types with support for selected inactive services.
  *
- * This model is generated automatically based on API.
- *
  * @augments WlSdk_ModelAbstract
  * @constructor
- * @see Wl\Service\ServiceList\List75Api
  */
 function Wl_Service_ServiceList_List75Model()
 {
@@ -21,54 +18,14 @@ function Wl_Service_ServiceList_List75Model()
    * @property {boolean} hide_application Whether service will be hidden in the White Label mobile application.
    *  <tt>true</tt> means that service will not be displayed, <tt>false</tt> otherwise.
    * @property {boolean} is_active Whether appointment type is active.
-   * @property {string} k_service Service key.
-   * @property {string} k_service_category Service category key.
-   * @property {string} text_category Service category name.
-   * @property {string} text_service Service name.
+   * @property {string} k_service Appointment type identifier, primary key.
+   * @property {string} k_service_category Appointment type category identifier, primary key.
+   * @property {string} text_category Appointment type category name.
+   * @property {string} text_service Appointment type name.
    */
 
   /**
-   * Services list.
-   *
-   * <dl>
-   *   <dt>
-   *     bool <var>hide_application</var>
-   *   </dt>
-   *   <dd>
-   *      Whether service will be hidden in the White Label mobile application.
-   *      <tt>true</tt> means that service will not be displayed, <tt>false</tt> otherwise.
-   *   </dd>
-   *   <dt>
-   *     bool <var>is_active</var>
-   *   </dt>
-   *   <dd>
-   *     Whether appointment type is active.
-   *   </dd>
-   *   <dt>
-   *     string <var>k_service</var>
-   *   </dt>
-   *   <dd>
-   *     Service key.
-   *   </dd>
-   *   <dt>
-   *     string <var>k_service_category</var>
-   *   </dt>
-   *   <dd>
-   *     Service category key.
-   *   </dd>
-   *   <dt>
-   *     string <var>text_category</var>
-   *   </dt>
-   *   <dd>
-   *     Service category name.
-   *   </dd>
-   *   <dt>
-   *     string <var>text_service</var>
-   *   </dt>
-   *   <dd>
-   *     Service name.
-   *   </dd>
-   * </dl>
+   * Appointment types list.
    *
    * @get result
    * @type {Wl_Service_ServiceList_List75Model_a_service[]}
@@ -76,8 +33,8 @@ function Wl_Service_ServiceList_List75Model()
   this.a_service = undefined;
 
   /**
-   * Whether to return franchisee-created services (if business is franchisor).
-   * <tt>true</tt> to include franchisee-created services.
+   * Whether to return franchisee-created appointment types (if business is franchisor).
+   * <tt>true</tt> to include franchisee-created appointment types.
    *
    * @get get
    * @type {boolean}
@@ -85,7 +42,7 @@ function Wl_Service_ServiceList_List75Model()
   this.is_franchise = false;
 
   /**
-   * Business key.
+   * Business key, primary key.
    *
    * @get get
    * @type {string}
@@ -109,8 +66,7 @@ Wl_Service_ServiceList_List75Model.prototype.config=function()
  * @function
  * @name Wl_Service_ServiceList_List75Model.instanceGet
  * @param {string} k_business Business key.
- * @param {boolean} is_franchise Whether to return franchisee-created services (if business is franchisor). <tt>true</tt> to include franchisee-created services.
+ * @param {boolean} is_franchise Whether to return franchisee-created appointment types (if business is franchisor). <tt>true</tt> to include franchisee-created appointment types.
  * @returns {Wl_Service_ServiceList_List75Model}
  * @see WlSdk_ModelAbstract.instanceGet()
  */
-
