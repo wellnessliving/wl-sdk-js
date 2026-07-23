871fbe87e6130c65a0e2a48a870c0429185d34de
sergiy <sidose4@gmail.com>
2026-07-23 17:01:18 +0300

    wl-82845: [Automated Marketing][programming] Add API endpoint to send abandoned_checkout event data from react widgets +review WL-65577

--- WellnessLiving/Wl/Widget/Analytics/WidgetAnalyticsEventModel.js
+++ WellnessLiving/Wl/Widget/Analytics/WidgetAnalyticsEventModel.js
@@ -10,16 +10,16 @@ function Wl_Widget_Analytics_WidgetAnalyticsEventModel()
 
   /**
    * @typedef {{}} Wl_Widget_Analytics_WidgetAnalyticsEventModel_a_payload_a_item
-   * @property {number} id_purchase_item A list of purchase types.
-   * @property {number} id_service A list of services.
-   * @property {string} k_item Selected item key. The referenced table depends on checkout type and item type.
-   * @property {string} k_item_extra Additional item key used by an event enrollment block. Empty for all other item types.
+   * @property {?number} id_purchase_item Purchase item ID.
+   * @property {?number} id_service Service ID.
+   * @property {?string} k_enrollment_block Enrollment block key for an event item. Empty for all other item types.
+   * @property {string} k_item Selected item key.
    */
 
   /**
    * @typedef {{}} Wl_Widget_Analytics_WidgetAnalyticsEventModel_a_payload
    * @property {Wl_Widget_Analytics_WidgetAnalyticsEventModel_a_payload_a_item} a_item Selected checkout items.
-   * @property {number} id_checkout_type Widget analytics checkout types.
+   * @property {number} id_checkout_type Checkout type.
    * @property {string} k_location Location key.
    * @property {?string} k_skin Widget skin key. `null` if the Widget does not use a registered skin.
    * @property {string} m_total Checkout total in the location currency.
@@ -41,20 +41,22 @@ function Wl_Widget_Analytics_WidgetAnalyticsEventModel()
   this.a_payload = undefined;
 
   /**
-   * Event schema version.
+   * Event schema version. Currently only {@link Wl_Widget_Analytics_WidgetAnalyticsEventVersionSid} is supported.
    *
    * @post post
+   * @see Wl_Widget_Analytics_WidgetAnalyticsEventVersionSid
    * @type {number}
    */
   this.i_event_version = 1;
 
   /**
-   * Whether the event was durably accepted for asynchronous processing.
+   * Event name. Initially only {@link Wl_Widget_Analytics_WidgetAnalyticsEventSid} is supported.
    *
-   * @post result
-   * @type {boolean}
+   * @post post
+   * @see Wl_Widget_Analytics_WidgetAnalyticsEventSid
+   * @type {number}
    */
-  this.is_accepted = undefined;
+  this.id_event_name = 0;
 
   /**
    * Business key used for shard and datacenter routing.
@@ -73,14 +75,6 @@ function Wl_Widget_Analytics_WidgetAnalyticsEventModel()
    */
   this.s_event_id = "";
 
-  /**
-   * Event name. Initially only `abandoned_checkout` is supported.
-   *
-   * @post post
-   * @type {string}
-   */
-  this.s_event_name = "";
-
   this.changeInit();
 }
 
@@ -91,5 +85,5 @@ WlSdk_ModelAbstract.extend(Wl_Widget_Analytics_WidgetAnalyticsEventModel);
  */
 Wl_Widget_Analytics_WidgetAnalyticsEventModel.prototype.config=function()
 {
-  return {"a_field":{"a_payload":{"post":{"post":true}},"i_event_version":{"post":{"post":true}},"is_accepted":{"post":{"result":true}},"k_business":{"post":{"post":true}},"s_event_id":{"post":{"post":true}},"s_event_name":{"post":{"post":true}}}};
+  return {"a_field":{"a_payload":{"post":{"post":true}},"i_event_version":{"post":{"post":true}},"id_event_name":{"post":{"post":true}},"k_business":{"post":{"post":true}},"s_event_id":{"post":{"post":true}}}};
 };
---
+++ WellnessLiving/Wl/Widget/Analytics/WidgetAnalyticsEventVersionSid.js
@@ -0,0 +1,16 @@
+/**
+ * Widget analytics event schema versions.
+ *
+ * Last ID: 1
+ */
+function Wl_Widget_Analytics_WidgetAnalyticsEventVersionSid()
+{
+  // Empty constructor.
+}
+
+/**
+ * Initial event schema.
+ *
+ * @type {number}
+ */
+Wl_Widget_Analytics_WidgetAnalyticsEventVersionSid.V1 = 1;
