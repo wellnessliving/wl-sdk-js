fbd7d6fd46ac5430393205f5f94db71d92e56214
sergiy <sidose4@gmail.com>
2026-07-27 20:30:05 +0300

    wl-82905: [Automated Marketing][programming] Add email for Abandoned Checkout trigger +review WL

--- WellnessLiving/Wl/Widget/Analytics/WidgetAnalyticsEventSid.js
+++ WellnessLiving/Wl/Widget/Analytics/WidgetAnalyticsEventSid.js
@@ -14,3 +14,17 @@ function Wl_Widget_Analytics_WidgetAnalyticsEventSid()
  * @type {number}
  */
 Wl_Widget_Analytics_WidgetAnalyticsEventSid.ABANDONED_CHECKOUT = 1;
+
+/**
+ * Begin checkout event.
+ *
+ * @type {number}
+ */
+Wl_Widget_Analytics_WidgetAnalyticsEventSid.BEGIN_CHECKOUT = 2;
+
+/**
+ * Purchase event.
+ *
+ * @type {number}
+ */
+Wl_Widget_Analytics_WidgetAnalyticsEventSid.PURCHASE = 3;
